5aff761eecda14635bc81c77fd2ac9f2cb093bd3
Carlos Henrique <carlosh.sales@outlook.com>
2022-08-21 02:50:52 -0300

    feat: add characterSchema

---
+++ database/mongoDB/schemas/characterSchema.js
@@ -0,0 +1,11 @@
+import { Schema, model } from "mongoose";
+
+const characterSchema = new Schema({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  userId: { type: String, required: true },
+  createAt: { type: Date, default: Date.now() }
+});
+
+export const characterModel = model("Character", characterSchema);
