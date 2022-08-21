25c8bc00e0f60f64f1b4c9d977520b4a3fb0dbe8
Carlos Henrique <carlosh.sales@outlook.com>
2022-08-21 02:52:29 -0300

    feat: getUser modify to findAllUser

--- services/usecases/user/getUser.js
+++
@@ -1,9 +0,0 @@
-export class GetUserUseCase {
-  constructor(userRepository) {
-    this.repository = userRepository;
-  }
-
-  async execute(user) {
-    return await this.repository.get(user)
-  }
-}
