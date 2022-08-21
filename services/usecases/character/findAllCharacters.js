bff8cb1061a5c508429a2653096585ac47d43e94
Carlos Henrique <carlosh.sales@outlook.com>
2022-08-21 00:52:30 -0300

    feat: add findAllCharacters useCase

---
+++ services/usecases/character/findAllCharacters.js
@@ -0,0 +1,9 @@
+export class FindAllCharactersUseCase {
+  constructor(characterRepository) {
+    this.repository = characterRepository;
+  }
+
+  async execute() {
+    return await this.repository.findAll();
+  }
+}
